3b2cd1284bed20f0059e5daf6265a743a02f56bf
andrejarboe <andrejarboe@gmail.com>
2022-10-02 19:43:37 -0400

    .env

---
+++ client/sanity/schemas/pageInfo.js
@@ -0,0 +1,44 @@
+export default {
+  name: 'pageInfo',
+  title: 'Page Info',
+  type: 'document',
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "role",
+      title: "Role",
+      type: "string",
+    },
+    {
+      name: "heroImage",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "heroInfo",
+      title: "Hero Info",
+      type: "text",
+    },
+    {
+      name: "profilePic",
+      title: "ProfilePic",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "socials",
+      title: "Socials",
+      type: "array",
+      of: [{ type: "reference", to: { type: "social" } }],
+    },
+  ]
+}
