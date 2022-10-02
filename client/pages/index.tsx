6be77ddf53f62124d8042b6e13e55c73d19b8f29
andrejarboe <andrejarboe@gmail.com>
2022-10-02 19:57:53 -0400

    add types to index

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,6 +1,21 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
+import { Experience, PageInfo, Project, Skill, Social } from "../typings";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchExperiences } from "../utils/fetchExperiences";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSocial } from "../utils/fetchSocials";
+
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[];
+};
+
 const Home: NextPage = () => {
   return (
     <div>
@@ -9,7 +24,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Andre Jarboe Software Engineer Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        
+
     </div>
   )
 }
