0f66e803e4bf7ed7b864b59616eaf0864cd73639
andrejarboe <andrejarboe@gmail.com>
2022-10-02 23:11:06 -0400

    require('@tailwindcss/typography'), npm i react-portable-text

--- client/pages/projects/[slug].tsx
+++ client/pages/projects/[slug].tsx
@@ -29,8 +29,32 @@ export default function projects({ project, socials }: Props) {
                 src={urlFor(project.image).url()!}
                 alt={project.title}
             /> */}
-            <article>
-                <h1 className=''>{project.title}</h1>
+            <article className="max-w-3xl mx-auto p-5">
+                <h1 className='text-3xl mt-10 mb-3'>{project.title}</h1>
+                <div className="mt-10">
+                    <PortableText
+                        className=""
+                        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                        content={post.body}
+                        serializers={{
+                            h1: (props: any) => (
+                                <h1 className="text-2xl font-bold my-5" {...props} />
+                            ),
+                            h2: (props: any) => (
+                                <h1 className="text-xl font-bold my-5" {...props} />
+                            ),
+                            li: ({ children }: any) => (
+                                <li className="ml-4 list-disc">{children}</li>
+                            ),
+                            link: ({ href, children }: any) => (
+                                <a href={href} className="text-blue-500 hover:underline">
+                                    {children}
+                                </a>
+                            ),
+                        }}
+                    />
+                </div>
             </article>
             <Footer socials={socials} />
 
